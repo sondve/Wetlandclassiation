da1cb1000635601c02be5662f556f44e973ccad4
Mycat <32902985+sondve@users.noreply.github.com>
2023-12-17 06:57:30 +0800

    Update Wetland_OO_function.js

--- Wetland_OO_function.js
+++ Wetland_OO_function.js
@@ -1,9 +1,9 @@
 // data转为image函数
-var createGLCBands = function(classValues, classNames, image, year) {
+var CreateBands = function(classValues, classNames, image, year) {
   return ee.Image(classValues.map(function(classValues, index) {
     var band = image.eq(classValues);
     return band.rename(classNames[index]);
   })).set('year', year);
 }
 
-exports.createGLCBands = createGLCBands;
+exports.CreateBands = CreateBands;
