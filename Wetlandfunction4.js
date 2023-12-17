7adeb539b0e77c3af0f96caa58f86583d051db4f
Mycat <32902985+sondve@users.noreply.github.com>
2023-12-17 21:28:17 +0800

    Update and rename Wetlandfunction3.js to Wetlandfunction4.js

--- Wetlandfunction4.js
+++ Wetlandfunction4.js
@@ -36,6 +36,40 @@ var reclassSamplefromSample = function(classSample, image) {
  return ReclassSample;
 }
 
+
+// 定义函数计算样本点每个波段的均值和标准差
+var calculateMeanAndStdDev = function(sample) {
+  var mean = sample.reduceColumns({
+    reducer: ee.Reducer.mean().repeat(7),
+    selectors: imgBand
+  });
+  mean = ee.List(ee.Dictionary(mean).get('mean'));
+
+  var stdDev = sample.reduceColumns({
+    reducer: ee.Reducer.stdDev().repeat(7),
+    selectors: imgBand
+  });
+  stdDev = ee.List(ee.Dictionary(stdDev).get('stdDev'));
+
+  var meanStdDevPlus = ee.List([0,1,2,3,4,5,6]).map(function(item){
+    var MeanStdDevPlus = ee.Number(mean.get(item)).add(stdDev.get(item));
+    return MeanStdDevPlus;
+  });
+  
+  var meanStdDevMinus = ee.List([0,1,2,3,4,5,6]).map(function(item){
+  var MeanStdDevMinus = ee.Number(mean.get(item)).subtract(stdDev.get(item));
+  return MeanStdDevMinus;
+  });
+
+  return {
+    mean: mean,
+    stdDev: stdDev,
+    meanStdDevPlus: meanStdDevPlus,
+    meanStdDevMinus: meanStdDevMinus
+  };
+}
+
 exports.CreateBands = CreateBands;
 exports.createLandCoverSample = createLandCoverSample;
 exports.reclassSamplefromSample = reclassSamplefromSample;
+exports.calculateMeanAndStdDev = calculateMeanAndStdDev;
